9eec82e5ae1af4b188f5ed876008f4c0c78c2990
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-09-26 00:29:25 +0200

    Added basic logic rigging.

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,15 @@
+use itertools::Itertools;
 use leptos::*;
+
+use crate::logic;
 
 #[component]
 pub fn App() -> impl IntoView {
     //let styler_class = stylers::style_sheet! {"./src/app/app.css"};
 
-    //t
+    let (_result, set_result) = create_signal(Vec::<logic::notes::SatbBlock>::new());
+
+    let (text_result, set_text_result) = create_signal("".to_string());
 
     view! { //class = styler_class,
         <h1>"Chorsatz"</h1>
@@ -17,7 +22,37 @@ pub fn App() -> impl IntoView {
         <div class="visible">
             <div>
                 <h2>Eingabe:</h2>
-                <input type="text" placeholder="Akkorde hier eingeben..."></input>
+                <input
+                    type="text"
+                    placeholder="Akkorde hier eingeben..."
+                    on:change=move |ev| {
+                        set_result({
+                            crate::logic::generate_satb(
+                                &event_target_value(&ev)
+                                .split(' ')
+                                .filter_map(|note| logic::notes::create_multinote(note).ok())
+                                .collect_vec()
+                            ).first().unwrap().0.clone()
+                        });
+
+                        set_text_result(
+                            crate::logic::generate_satb(
+                                &event_target_value(&ev)
+                                .split(' ')
+                                .filter_map(|note| logic::notes::create_multinote(note).ok())
+                                .collect_vec()
+                            ).first().unwrap().0.iter().map(|block| block.to_string()).join(" - ")
+                        );
+                    }
+                    prop:value=""
+                ></input>
+            </div>
+        </div>
+
+        <div class="visible">
+            <div>
+                <h2>Ausgabe:</h2>
+                <p>{text_result}</p>
             </div>
         </div>
     }
