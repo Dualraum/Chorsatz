ed7bc007ba4cb8a5d08a27afc090a24abeb937e8
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-09-28 01:28:34 +0200

    Added dummy collapsibles.

--- src/app.rs
+++ src/app.rs
@@ -12,6 +12,13 @@ pub fn App() -> impl IntoView {
 
     let (input, set_input) = create_signal(String::new());
 
+    let (options, set_options) = create_signal(false);
+    let options_class = move || if options() { "col_open" } else { "col_closed" };
+    let options_active = move || if options() { "active" } else { "" };
+    let (accords, set_accords) = create_signal(false);
+    let accords_class = move || if accords() { "col_open" } else { "col_closed" };
+    let accords_active = move || if accords() { "active" } else { "" };
+
     view! { //class = styler_class,
         <h1>"Chorsatz"</h1>
         <div class = "outer_block">
@@ -44,16 +51,39 @@ pub fn App() -> impl IntoView {
                 <button id="generate"
                     on:click=move |_| {
                         crate::logic::generate_satb(
-                            &input
-                            .get()
+                            &input()
                             .split(' ')
                             .filter_map(|note| logic::notes::create_multinote(note).ok())
                             .collect_vec()
                         ).into_iter().take(5).collect_vec();
                     }
                 >"Generieren"</button>
-                <button id="options">"Optionen"</button>
-                <button id="accords">"Akkorde"</button>
+                <button id="options" class={options_active}
+                    on:click=move |_|{
+                        set_options(!options());
+                        if options() {
+                            set_accords(false);
+                        }
+                    }
+                >"Optionen"</button>
+                <button id="accords" class={accords_active}
+                    on:click=move|_|{
+                        set_accords(!accords());
+                        if accords() {
+                            set_options(false);
+                        }
+                    }
+                >"Akkorde"</button>
+            </div>
+            <div class={options_class}>
+                    <p>
+                        "Hier werden in Zukunft mögliche Optionen zur Generierung stehen."
+                    </p>
+            </div>
+            <div class={accords_class}>
+                    <p>
+                        "Hier wird in Zukunft eine Tabelle der Akkordabkürzungen stehen."
+                    </p>
             </div>
             <p class="deemph">
                 "Verfügbar sind sämtliche Dur- und Moll-Dreiklänge sowie verminderte, übermäßige, sus2 und sus4  Versionen dieser Akkorde.
