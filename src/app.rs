adf7c6fd8cf9d26d43cc8a371693d721ad788ff0
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-10-04 17:46:07 +0200

    Inlined some derived attributes.

--- src/app.rs
+++ src/app.rs
@@ -20,11 +20,7 @@ pub fn App() -> impl IntoView {
     let (input, set_input) = create_signal(String::new());
 
     let (options, set_options) = create_signal(false);
-    let options_class = move || if options() { "col_open" } else { "col_closed" };
-    let options_active = move || if options() { "active" } else { "" };
     let (accords, set_accords) = create_signal(false);
-    let accords_class = move || if accords() { "col_open" } else { "col_closed" };
-    let accords_active = move || if accords() { "active" } else { "" };
 
     view! { //class = styler_class,
         <h1>"Chorsatz"</h1>
@@ -76,7 +72,7 @@ pub fn App() -> impl IntoView {
                         set_accords(false);
                     }
                 >"Generieren"</button>
-                <button id="options" class={options_active}
+                <button id="options" class=move || if options() { "active" } else { "" }
                     on:click=move |_|{
                         set_options.update(|opt| *opt = !*opt);
                         if options() {
@@ -84,7 +80,7 @@ pub fn App() -> impl IntoView {
                         }
                     }
                 >"Optionen"</button>
-                <button id="accords" class={accords_active}
+                <button id="accords" class=move || if accords() { "active" } else { "" }
                     on:click=move|_|{
                         set_accords.update(|acc| *acc = !*acc);
                         if accords() {
@@ -93,10 +89,10 @@ pub fn App() -> impl IntoView {
                     }
                 >"Eingabehilfe"</button>
             </div>
-            <div class={options_class}>
+            <div class=move || if options() { "col_open" } else { "col_closed" }>
                     <options::Options config=config set_config=set_config/>
             </div>
-            <div class={accords_class}>
+            <div class=move || if accords() { "col_open" } else { "col_closed" }>
                     <note_info::Info/>
             </div>
             </div>
