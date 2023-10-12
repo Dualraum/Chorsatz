4676a2b8647654e428a466f8b648b93c35cd12e2
Linus-Mussmaecher <linus.mussmaecher@gmail.com>
2023-10-12 12:09:48 +0200

    Added basic svgs.

--- src/app/result_view.rs
+++ src/app/result_view.rs
@@ -16,7 +16,7 @@ pub fn SatbResultView(result: Vec<SatbBlock>, res_score: f32, index: usize) -> i
                 <a
                     class="dl"
                     href={format!("data:text/plain;charset=utf-8,{}", encode_uri_component(&crate::logic::generate_hum_file(&result)))}
-                    download={format!("SATB-Result{}.hum", index)}
+                    download={format!("SATB-Result{}.hum", index+1)}
                 >
                     ".hum"
                 </a>
@@ -24,7 +24,7 @@ pub fn SatbResultView(result: Vec<SatbBlock>, res_score: f32, index: usize) -> i
                 <a
                     class="dl"
                     href={format!("data:text/plain;charset=utf-8,{}", encode_uri_component(&crate::logic::generate_hum_file(&result)))}
-                    download={format!("SATB-Result{}.ly", index)}
+                    download={format!("SATB-Result{}.ly", index+1)}
                 >
                     ".ly (WIP)"
                 </a>
